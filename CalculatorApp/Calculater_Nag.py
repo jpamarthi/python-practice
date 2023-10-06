726f3a7f4d860109c450e3b56f743053508c68f4
nagarajumareedu <nagarajugec27@gmail.com>
2023-10-06 16:25:52 +0530

    mycaculatorapp

---
+++ CalculatorApp/Calculater_Nag.py
@@ -0,0 +1,14 @@
+def add_numbers(a, b):
+    return a + b
+
+
+def subtract_numbers(a, b):
+    return a - b
+
+
+def divide_numbers(a, b):
+    return a / b
+
+
+def multipli_numbers(a, b):
+    return a * b
