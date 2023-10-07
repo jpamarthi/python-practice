22b9258a0539cb38d9fa132129e4adf328347e52
saran740 <146550550+saran740@users.noreply.github.com>
2023-10-07 08:36:11 -0400

    fixed test cases by removing self

--- Pytest/TestCalculator_Nag.py
+++ Pytest/TestCalculator_Nag.py
@@ -2,20 +2,20 @@ import pytest
 from CalculatorApp.Calculater_Nag import *
 
 
-def test_subtract_numbers(self):
+def test_subtract_numbers():
     assert subtract_numbers(8, 6) == 2
 
 
-def test_add_numbers(self):
+def test_add_numbers():
     assert add_numbers(2, 3) == 5
     assert add_numbers(5, 7) == 12
 
 
-def test_divide_numbers(self):
+def test_divide_numbers():
     assert divide_numbers(4, 2) == 2
     assert divide_numbers(10, 2) == 5
 
 
-def test_multipli_numbers(self):
+def test_multipli_numbers():
     assert multipli_numbers(2, 3) == 6
     assert multipli_numbers(5, 6) == 30
