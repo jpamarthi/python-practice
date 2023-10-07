d63e3189fbda1ff69c32f0b6f33c199625b0f219
jass9 <786jassibrar@gmail.com>
2023-10-07 08:15:00 -0400

    Pytest Calculator App

---
+++ Pytest/TestCalculatorApp.py
@@ -0,0 +1,21 @@
+import pytest
+from CalculatorApp.Calculator_Jass import *
+
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(5, 7) == 12
+
+
+def test_subtract_numbers():
+    assert subtract_numbers(8, 6) == 2
+
+
+def test_divide_numbers():
+    assert divide_numbers(4, 2) == 2
+    assert divide_numbers(10, 2) == 5
+
+
+def test_multipli_numbers():
+    assert multipli_numbers(2, 3) == 6
+    assert multipli_numbers(5, 6) == 30
