20cd5d1d35dfa2e66e43b14a6f7fed07a4a084d2
saran740 <146550550+saran740@users.noreply.github.com>
2023-10-21 06:22:52 -0400

    Added comment Saran

--- Examples/CalculatorApp/calculatorSaran.py
+++ Examples/CalculatorApp/calculatorSaran.py
@@ -1,19 +1,24 @@
+# This Method Adds Two values
 def add_numbers(a, b):
     return a + b
 
 
+# This Method Divides Two values
 def divide_numbers(a, b):
     return a / b
 
 
+# This Method Subtracts Two values
 def subtract_numbers(a, b):
     return a - b
 
 
+# This Method Multiply Two values
 def multipli_numbers(a, b):
     return a * b
 
 
+# This Method Returns the Reminder when we do division for Two values
 def modulo_numbers(a, b):
     return a % b
 
--- Examples/MilesAndKilometersApp/ConversionSaran.py
+++ Examples/MilesAndKilometersApp/ConversionSaran.py
@@ -1,6 +1,8 @@
+# This Method Returns Conversion of Miles To Kilometers
 def miles_to_kilometers(miles):
     return round(miles * 1.61, 2)
 
 
+# This Method Returns Conversion of Kilometers to Miles
 def kilometers_to_miles(kilometers):
     return round(kilometers / 1.61, 2)
