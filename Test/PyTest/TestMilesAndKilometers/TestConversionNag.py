b8589c66e9110938c5f759e150aa6f4f9cfee8b0
nagarajumareedu <nagarajugec27@gmail.com>
2023-10-21 05:51:16 +0530

    added miles to kilometers app

---
+++ Test/PyTest/TestMilesAndKilometers/TestConversionNag.py
@@ -0,0 +1,12 @@
+import pytest
+from Examples.MilesAndKilometersApp.ConversionNag import *
+
+
+def test_kilometers_to_miles():
+    assert kilometers_to_miles(2) == 1.24
+    assert kilometers_to_miles(5) == 3.11
+
+
+def test_miles_to_kilometers():
+    assert miles_to_kilometers(2) == 3.22
+    assert miles_to_kilometers(5) == 8.05
