322f87976a75594ede4421bdeadaea159c530b80
jass9 <786jassibrar@gmail.com>
2023-10-21 06:24:34 -0400

    Added Comments

--- Examples/MilesAndKilometersApp/ConversionJass.py
+++ Examples/MilesAndKilometersApp/ConversionJass.py
@@ -1,7 +1,9 @@
+# This method returns conversion of Miles to Kilometers
 def miles_to_kilometers(miles):
     return round(miles * 1.61, 2)
 
 
+# This method returns conversion kilometers to miles
 def kilometers_to_miles(kilometers):
     return round(kilometers / 1.61, 2)
 
