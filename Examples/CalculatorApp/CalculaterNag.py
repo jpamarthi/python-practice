bfccf31f0195db67808dfbbca0db9a16d7b068cb
nagarajumareedu <nagarajugec27@gmail.com>
2023-10-21 16:22:52 +0530

    Added comments in all the apps

--- Examples/CalculatorApp/CalculaterNag.py
+++ Examples/CalculatorApp/CalculaterNag.py
@@ -1,14 +1,18 @@
+# This Method Add Two values
 def add_numbers(a, b):
     return a + b
 
 
+# This Method Subtracts Two values
 def subtract_numbers(a, b):
     return a - b
 
 
+# This Method Divides Two values
 def divide_numbers(a, b):
     return a / b
 
 
+# This Methode multiply Two values
 def multipli_numbers(a, b):
     return a * b
--- Examples/MilesAndKilometersApp/ConversionNag.py
+++ Examples/MilesAndKilometersApp/ConversionNag.py
@@ -1,7 +1,9 @@
+# This method returns conversion of Miles to kilometers
 def miles_to_kilometers(miles):
     return round(miles * 1.61, 2)
 
 
+# This method returns conversion of Kilometers to Miles
 def kilometers_to_miles(kilometers):
     return round(kilometers / 1.61, 2)
 
