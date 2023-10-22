7e1a45e57582b5aaccf307a660dd0ea65c153f7d
jass9 <786jassibrar@gmail.com>
2023-10-22 09:20:41 -0400

    fixed code smells

--- Examples/MilesAndKilometersApp/ConversionJass.py
+++ Examples/MilesAndKilometersApp/ConversionJass.py
@@ -1,9 +1,9 @@
 # This method returns conversion of Miles to Kilometers
 def miles_to_kilometers(miles):
-    return round(miles * 1.61, 2)
+    return round(miles * 1.61)
 
 
 # This method returns conversion kilometers to miles
 def kilometers_to_miles(kilometers):
-    return round(kilometers / 1.61, 2)
+    return round(kilometers / 1.61)
 
--- Test/PyTest/TestMilesAndKilometers/TestCoversionJass.py
+++ Test/PyTest/TestMilesAndKilometers/TestCoversionJass.py
@@ -3,10 +3,10 @@ from Examples.MilesAndKilometersApp.ConversionJass import *
 
 
 def test_kilometers_to_miles():
-    assert kilometers_to_miles(2) == 1.24
-    assert kilometers_to_miles(5) == 3.11
+    assert kilometers_to_miles(2) == 1
+    assert kilometers_to_miles(5) == 3
 
 
 def test_miles_to_kilometers():
-    assert miles_to_kilometers(2) == 3.22
-    assert miles_to_kilometers(5) == 8.05
+    assert miles_to_kilometers(2) == 3
+    assert miles_to_kilometers(5) == 8
