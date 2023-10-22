6907169ab8af5047374fea1b67146f09407c6a6c
saran740 <146550550+saran740@users.noreply.github.com>
2023-10-22 09:45:10 -0400

    Modified test

--- Examples/MilesAndKilometersApp/ConversionSaran.py
+++ Examples/MilesAndKilometersApp/ConversionSaran.py
@@ -1,8 +1,8 @@
 # This Method Returns Conversion of Miles To Kilometers
 def miles_to_kilometers(miles):
-    return round(miles * 1.61, 2)
+    return round(miles * 1.61)
 
 
 # This Method Returns Conversion of Kilometers to Miles
 def kilometers_to_miles(kilometers):
-    return round(kilometers / 1.61, 2)
+    return round(kilometers / 1.61)
--- Test/PyTest/TestMilesAndKilometers/TestCoversionSaran.py
+++ Test/PyTest/TestMilesAndKilometers/TestCoversionSaran.py
@@ -3,11 +3,11 @@ from Examples.MilesAndKilometersApp.ConversionSaran import miles_to_kilometers,k
 
 
 def test_kilometers_to_miles():
-    assert kilometers_to_miles(2) == 1.24
-    assert kilometers_to_miles(5) == 3.11
+    assert kilometers_to_miles(2) == 1
+    assert kilometers_to_miles(5) == 3
 
 
 def test_miles_to_kilometers():
-    assert miles_to_kilometers(2) == 3.22
-    assert miles_to_kilometers(5) == 8.05
+    assert miles_to_kilometers(2) == 3
+    assert miles_to_kilometers(5) == 8
 
