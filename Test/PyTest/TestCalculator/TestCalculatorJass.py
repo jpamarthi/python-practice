864c74ad61229e727b31d088a74ce60050bfdc59
jass9 <786jassibrar@gmail.com>
2023-10-22 09:12:20 -0400

    fixed importing file

--- Test/PyTest/TestCalculator/TestCalculatorJass.py
+++ Test/PyTest/TestCalculator/TestCalculatorJass.py
@@ -1,5 +1,5 @@
 import pytest
-from Examples.CalculatorApp.CalculatorJass import *
+from Examples.CalculatorApp.CalculatorJass import add_numbers, subtract_numbers, divide_numbers, multipli_numbers, exponentiation_number
 
 
 def test_add_numbers():
