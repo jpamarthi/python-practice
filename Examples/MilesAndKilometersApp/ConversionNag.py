af389cac1197cb535bd8187027dcdd0b42fc3b77
nagarajumareedu <nagarajugec27@gmail.com>
2023-10-22 19:16:24 +0530

    Tesh Miles and kilometers were removed by desimals

--- Examples/MilesAndKilometersApp/ConversionNag.py
+++ Examples/MilesAndKilometersApp/ConversionNag.py
@@ -1,9 +1,9 @@
 # This method returns conversion of Miles to kilometers
 def miles_to_kilometers(miles):
-    return round(miles * 1.61, 2)
+    return round(miles * 1.61)
 
 
 # This method returns conversion of Kilometers to Miles
 def kilometers_to_miles(kilometers):
-    return round(kilometers / 1.61, 2)
+    return round(kilometers / 1.61)
 
