13f47757aee09ec60fd91152505dcf9c97300ea2
nagarajumareedu <nagarajugec27@gmail.com>
2023-10-24 14:35:53 +0530

    Created the Larger number

---
+++ Examples/FindLargestNumber/FindLargestNumberNag.py
@@ -0,0 +1,7 @@
+def find_largest_two_numbers(number1, number2):
+    # Choose the larger number
+    if number1 > number2:
+        larger_number = number1
+    else:
+        larger_number = number2
+    return larger_number
