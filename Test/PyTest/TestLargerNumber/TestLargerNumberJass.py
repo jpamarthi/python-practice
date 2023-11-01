c4bf0ad28675a55af9c9b4d8852610ea71669df2
jass9 <786jassibrar@gmail.com>
2023-11-01 07:28:59 -0400

    find largest of 3 numbers

--- Test/PyTest/TestLargerNumber/TestLargerNumberJass.py
+++ Test/PyTest/TestLargerNumber/TestLargerNumberJass.py
@@ -1,7 +1,13 @@
 import pytest
-from Examples.FindLargestNumber.FindLargestNumberJass import find_largest_two_numbers
+from Examples.FindLargestNumber.FindLargestNumberJass import find_largest_two_numbers, find_largest_three_numbers
 
 
 def test_find_largest_two_numbers():
     assert find_largest_two_numbers(100, 200) == 200
     assert find_largest_two_numbers(300, 200) == 300
+
+
+def test_find_largest_three_numbers():
+    assert find_largest_three_numbers(100, 200, 300) == 300
+    assert find_largest_three_numbers(300, 100, 200) == 300
+    assert find_largest_three_numbers(100, 300, 200) == 300
