27b2d6440a56889f1bf47bdf675403ddf53f53f0
sarikajagadish <149593180+sarikajagadish@users.noreply.github.com>
2023-11-05 05:52:41 -0600

    Added calculator app

---
+++ Test/PyTest/TestCalculator/TestCalculatorSarika.py
@@ -0,0 +1,28 @@
+import pytest
+from Examples.CalculatorApp.CalculatorJass import add_numbers, subtract_numbers, divide_numbers, multipli_numbers, \
+    exponentiation_number
+
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(5, 7) == 12
+    assert add_numbers(5, 4) == 9
+
+
+def test_subtract_numbers():
+    assert subtract_numbers(8, 6) == 2
+
+
+def test_divide_numbers():
+    assert divide_numbers(4, 2) == 2
+    assert divide_numbers(10, 2) == 5
+
+
+def test_multipli_numbers():
+    assert multipli_numbers(2, 3) == 6
+    assert multipli_numbers(5, 6) == 30
+
+
+def test_exponentiation_numbers():
+    assert exponentiation_number(2, 3) == 8
+    assert exponentiation_number(3, 3) == 27
