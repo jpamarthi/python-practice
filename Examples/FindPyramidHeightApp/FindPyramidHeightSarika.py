e87d70c14e8bbfbe773bcff3a1b5414ba67b18e7
jass9 <786jassibrar@gmail.com>
2023-11-05 09:58:44 -0500

    modified

--- Examples/FindPyramidHeightApp/FindPyramidHeightSarika.py
+++ Examples/FindPyramidHeightApp/FindPyramidHeightSarika.py
@@ -24,27 +24,15 @@ def find_pyramid_height(blocks):
 
     Expected output: The height of the pyramid: 1
     """
-    # height = 0
-    # while a > 0:
-    #     height += 1
-    #     a -= height
-    # return height
-    # 2. Assign the value of height to 0
-
+    # 1. Assign the value of height to 0
     height = 0
 
-    # 3. Assign the value of num_of_blocks to 0
-    num_of_blocks = 0
-
-    # 4. Loop condition to check weather the block is not equal to zero and blocks >= height + 1
+    # 2. Loop condition to check weather the block is not equal to zero and blocks >= height + 1
     while (blocks != 0) and (blocks >= height + 1):
-        # 4a. increment height
+        # 2a. increment height
         height += 1
 
-        # 4b. add num_of_blocks + height
-        num_of_blocks += height
-
-        # 4c. sub blocks - height
+        # 2b. sub blocks - height
         blocks -= height
 
     # print("The height of the pyramid:", height)
