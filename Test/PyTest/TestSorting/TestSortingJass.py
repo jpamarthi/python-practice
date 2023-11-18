48de8363ef1f02ac9c49ecfdbb75b4c8b38dbad6
jass9 <786jassibrar@gmail.com>
2023-11-18 07:28:52 -0500

    jass sorting app

--- Test/PyTest/TestSorting/TestSortingJass.py
+++ Test/PyTest/TestSorting/TestSortingJass.py
@@ -3,4 +3,4 @@ from Examples.SortingApp.SortingSarika import sorting_list_bubble_sort
 
 
 def test_sorting_list_bubble_sort():
-    assert sorting_list_bubble_sort([3, 6, 4, 2, 1]) == [1, 2, 3, 4, 6]
+    assert sorting_list_bubble_sort([3, 8, 4, 2, 1]) == [1, 2, 3, 4, 8]
