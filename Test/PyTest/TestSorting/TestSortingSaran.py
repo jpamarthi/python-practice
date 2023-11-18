c5d546d79441024e34bec59f22e1aecbdd787a64
saran740 <146550550+saran740@users.noreply.github.com>
2023-11-18 07:22:10 -0500

    Sorting modified

--- Test/PyTest/TestSorting/TestSortingSaran.py
+++ Test/PyTest/TestSorting/TestSortingSaran.py
@@ -3,4 +3,4 @@ from Examples.SortingApp.SortingSaran import sorting_list_bubble_sort
 
 
 def test_sorting_list_bubble_sort():
-    assert sorting_list_bubble_sort([3, 6, 4, 2, 1]) == [1, 2, 3, 4, 6]
+    assert sorting_list_bubble_sort([3, 6, 5, 2, 1]) == [1, 2, 3, 5, 6]
