d11388b36e36a9e34e01c87a5c54ab2b4f906371
jass9 <786jassibrar@gmail.com>
2023-12-27 06:48:39 -0500

    test for sum in list

---
+++ Examples/InterviewProblemsApp/InterviewProblemsJass.py
@@ -0,0 +1,12 @@
+def find_sum_in_list(lst, s):
+    flag = False
+    # Step 1: Iterate each number in the outer loop
+    for i in range(0, len(lst) - 1):
+        # Step 2: Iterate through the each number inner loop
+        for j in range(i + 1, len(lst)):
+            # Step 3: Add two numbers and see if we are getting to the sum
+            if lst[i] + lst[j] == s:
+                flag = True
+                break
+    # Step 4: Return True or False
+    return flag
