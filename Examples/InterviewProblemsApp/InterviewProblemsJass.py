d1522357be39a02564c70a06e7cd50a09a7d039a
jass9 <786jassibrar@gmail.com>
2023-12-27 06:58:03 -0500

    modifies sum in list file

--- Examples/InterviewProblemsApp/InterviewProblemsJass.py
+++ Examples/InterviewProblemsApp/InterviewProblemsJass.py
@@ -1,12 +1,10 @@
 def find_sum_in_list(lst, s):
-    flag = False
     # Step 1: Iterate each number in the outer loop
     for i in range(0, len(lst) - 1):
-        # Step 2: Iterate through the each number inner loop
+        # Step 2: Iterate through each number inner loop
         for j in range(i + 1, len(lst)):
             # Step 3: Add two numbers and see if we are getting to the sum
             if lst[i] + lst[j] == s:
-                flag = True
-                break
+                return True
     # Step 4: Return True or False
-    return flag
+    return False
