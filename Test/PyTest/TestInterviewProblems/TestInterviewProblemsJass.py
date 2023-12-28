e852cf8b3d2975205966e69602131ade9943d511
jass9 <786jassibrar@gmail.com>
2023-12-28 07:15:49 -0500

    find sum approach 2

--- Test/PyTest/TestInterviewProblems/TestInterviewProblemsJass.py
+++ Test/PyTest/TestInterviewProblems/TestInterviewProblemsJass.py
@@ -1,7 +1,12 @@
 import pytest
-from Examples.InterviewProblemsApp.InterviewProblemsJass import find_sum_in_list
+from Examples.InterviewProblemsApp.InterviewProblemsJass import find_sum_in_list, find_sum_in_lst_approach2
 
 
 def test_find_sum_in_list():
     assert not find_sum_in_list([1, 2, 4, 9], 7)
     assert find_sum_in_list([1, 2, 4, 8], 10)
+
+
+def test_find_sum_in_lst_approach2():
+    assert not find_sum_in_lst_approach2([1, 2, 4, 9], 7)
+    assert find_sum_in_lst_approach2([1, 2, 4, 8], 10)
