37cb72c2f12f6645eafd44697775dcf2ec2c8ad5
nagarajumareedu <146553130+nagarajumareedu@users.noreply.github.com>
2023-12-29 18:09:59 +0530

    Added approach 3 which needs to be fixed

--- Examples/InterviewProblemsApp/InterviewProblemsNag.py
+++ Examples/InterviewProblemsApp/InterviewProblemsNag.py
@@ -8,3 +8,25 @@ def find_sum_in_list(lst, s):
                 return True
     # Step 4: Return True or False
     return False
+
+
+def find_sum_in_list_3(lst, s):
+    i = 0
+    j = -1
+    # Step 1: Iterate through the list from both directions
+    while True:
+        # Step 2: Add the first and last elements
+        result_sum = lst[i] + lst[j]
+        # Step 3: Check if the result is greater or lesser or equal
+        # Step 4: If it is equal return True
+        if result_sum == s:
+            return True
+        # Step 5: If the sum is greater, then move the ending pointer backwards
+        elif result_sum > s:
+            j -= 1
+        # Step 6: If the sum is less, then move the starting pointer forward
+        else:
+            i += 1
+
+    # Step 7: Return False since there are no matches
+    return False
