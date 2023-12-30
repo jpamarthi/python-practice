e0bf06380e364eef90cecc2dad2495210c979ccc
saran740 <146550550+saran740@users.noreply.github.com>
2023-12-30 07:34:22 -0500

    Modified

--- Test/PyTest/TestInterviewProblems/TestInterViewProblemsSaran.py
+++ Test/PyTest/TestInterviewProblems/TestInterViewProblemsSaran.py
@@ -3,10 +3,10 @@ from Examples.InterviewProblemsApp.InterViewProblemsSaran import find_sum_in_lis
 
 
 def test_find_sum_in_list():
-    assert not find_sum_in_list([5, 3, 4, 8], 6)
-    assert find_sum_in_list([1, 2, 5, 10], 12)
+    assert not find_sum_in_list([1, 2, 3, 5, 8], 15)
+    assert find_sum_in_list([1, 5, 6, 6, 7], 12)
 
 
 def test_find_sum_in_lst_approach2():
-    assert not find_sum_in_lst_approach2([5, 3, 4, 8], 6)
-    assert find_sum_in_lst_approach2([1, 2, 5, 10], 12)
+    assert not find_sum_in_lst_approach2([1, 2, 3, 5, 8], 15)
+    assert find_sum_in_lst_approach2([1, 5, 6, 6, 7], 12)
